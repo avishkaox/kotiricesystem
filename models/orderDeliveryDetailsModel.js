620bdb1c09cde759660ecc07a480703cbe895df8
avishkarox <avishka.madushanka123@gmail.com>
2024-03-23 16:24:14 +0530

    orderadress details

--- models/orderDeliveryDetailsModel.js
+++ models/orderDeliveryDetailsModel.js
@@ -7,41 +7,6 @@ const deliverySchema = mongoose.Schema(
       required: true,
       ref: "clientUser",
     },
-    orderid: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "PurchasedProduct",
-    },
-    title: {
-      type: String,
-      enum: ["Mr", "Mrs", "Miss", "Ms", "Dr"],
-      required: [true, "Select Your Title"],
-      trim: true,
-    },
-    firstname: {
-      type: String,
-      required: [true, "Please add a First Name"],
-      trim: true,
-    },
-    lastname: {
-      type: String,
-      required: [true, "Please add a Last Name"],
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: [true, "Please enter a Email address"],
-      unique: true,
-      trim: true,
-      match: [
-        /^\w+([\\.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-        "Please enter a valid email address",
-      ],
-    },
-    phone: {
-      type: String,
-      trim: true,
-    },
     houseno: {
       type: String,
     },
@@ -57,5 +22,5 @@ const deliverySchema = mongoose.Schema(
   }
 );
 
-const Delivery = mongoose.model("Delivery", deliverySchema);
+const Delivery = mongoose.model("delivery", deliverySchema);
 module.exports = Delivery;
